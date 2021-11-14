9ac9ecf2d1f93cd6266e6a4390fb3886b5190c95
Robert <91354477+rwaltersii@users.noreply.github.com>
2021-11-14 16:53:03 -0500

    first commit

---
+++ TestFunctions.js
@@ -0,0 +1,21 @@
+// function setToRandom(scale) {
+//   return {
+//     x: Math.random() * scale,
+//     y: Math.random() * scale,
+//   };
+// }
+
+// let xy = setToRandom(10);
+// console.log(xy.x);
+// console.log(xy.y);
+
+// let x = 10;
+// console.log((x = -x));
+
+// let directionX = 1;
+// directionX = !directionX;
+// console.log(directionX);
+
+let x = 10;
+let y = 2;
+console.log((x -= y));
